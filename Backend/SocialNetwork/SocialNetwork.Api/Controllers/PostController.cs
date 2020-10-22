a24e2bcfceb4432ad07816ea99f1a0b7e663c6aa
EdwardCadena <earodriguez154@misena.edu.co>
2020-10-22 02:56:27 -0500

    The project architecture is done.

---
+++ Backend/SocialNetwork/SocialNetwork.Api/Controllers/PostController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialNetwork.Infrastructure.Repositories;
+
+namespace SocialNetwork.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetPost()
+        {
+            var post = new PostRepository().GetPosts();
+            return Ok(post);
+        }
+    }
+}
---
+++ Backend/SocialNetwork/SocialNetwork.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,24 @@
+﻿using SocialNetwork.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SocialNetwork.Infrastructure.Repositories
+{
+    public class PostRepository
+    {
+        public IEnumerable<Post> GetPosts()
+        {
+            var posts = Enumerable.Range(1, 10).Select(x => new Post
+            {
+                PostId = x,
+                Description = $"Description {x}",
+                Date = DateTime.Now,
+                Image = $"https://ticjob.co/es/job/desarrollador-net-wcf/8466?utm_source=neuvoo&utm_medium=ppc&utm_campaign=medium{x}",
+                UserId = x * 2
+            });
+            return posts;
+        }
+    }
+}
